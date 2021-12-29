33178928c93ed6cbbf91bb6d690f2502d3a86d25
ezenacapi <ezenacapi@gmail.com>
2021-12-29 11:48:22 +0900

    인터페이스 implement 구조 변경

--- src/kr/co/ezenac/ioc/Base64Encoder.java
+++ src/kr/co/ezenac/ioc/Base64Encoder.java
@@ -2,8 +2,9 @@ package kr.co.ezenac.ioc;
 
 import java.util.Base64;
 
-public class Encoder implements IEncoder {
+public class Base64Encoder implements IEncoder {
 
+    @Override
     public String encode(String message) {
         return Base64.getEncoder().encodeToString(message.getBytes());
     }
